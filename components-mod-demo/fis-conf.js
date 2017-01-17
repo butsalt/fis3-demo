5af3bbf4afaa8f9d2c3565af7e35a84ef8c2e57d
YuHeng Sun <butsalt@163.com>
2017-01-17 09:51:29 +0800

    add comment

--- components-mod-demo/fis-conf.js
+++ components-mod-demo/fis-conf.js
@@ -16,6 +16,7 @@ fis.match('::package', {
     // npm install [-g] fis3-postpackager-loader
     // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
     postpackager: fis.plugin('loader', {
+        // 资源表格式
         resourceType: 'commonJs',
         useInlineMap: true // 资源映射表内嵌
     })
